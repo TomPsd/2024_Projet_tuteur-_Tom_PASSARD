9715faeb86125104b9c386165314553dd93e728a
Stagiaire <Stagiaire@NCY-LEF-ANIEDZW.inra.local>
2024-05-22 16:56:05 +0200

    mise à jour du script du 22 mai

--- UN_Comtrade_data_import.r
+++ UN_Comtrade_data_import.r
@@ -60,32 +60,9 @@ write.csv(trade_data,
           "./raw_data/data_440131.csv",
           row.names = FALSE)
 
-# To load back the csv file
-data = read.csv2("./raw_data/data_440131.csv",sep = ",")
-
-###data<-read.csv(list.files(path = "./raw_data",
-   #                       pattern = ".csv",
-   #                       full.names = TRUE))
-
 all(dim(data) == dim(trade_data)) # if TRUE: same dimensions
 # The two dataframes are not exactly equals: some minor differences remain
 # To check differences, use: all.equal(data, trade_data)
-
-
-
-###Jointure des deux tableaux pour observer les données miroir###
-##On remarque qu'il y'a de nombreuses NA dans le jeu miroir
-export_440131 = data[(data$flowCode == "X"),]
-import_440131 = data[(data$flowCode == "M"),]
-
-mirror_flow_440131 = merge(x= export_440131[c("cmdCode","period","reporterDesc","partnerDesc","primaryValue")],
-                    y= import_440131[c("cmdCode","period","reporterDesc","partnerDesc","primaryValue")],
-                    by.x=c("cmdCode", "period", "reporterDesc", "partnerDesc"),
-                    by.y=c("cmdCode", "period", "partnerDesc", "reporterDesc"),
-                    all.x = TRUE, all.y = TRUE)
-View(mirror_flow_440131)
-sum(is.na(mirror_flow_440131))
-
 
 #####IMPOTATION DU JEU DE DONNEES 4418#####
 
@@ -136,25 +113,9 @@ write.csv(trade_data_2,
           "./raw_data/data_4418.csv",
           row.names = FALSE)
 
-# To load back the csv file
-data2 = read.csv2("./raw_data/data_4418.csv",sep = ",")
-
 all(dim(data2) == dim(trade_data_2)) # if TRUE: same dimensions
 # The two dataframes are not exactly equals: some minor differences remain
 # To check differences, use: all.equal(data, trade_data)
-
-###Jointure des deux tableaux pour observer les données miroir###
-##On remarque qu'il y'a de nombreuses NA dans le jeu miroir
-export_4418 = data2[(data2$flowCode == "X"),]
-import_4418 = data2[(data2$flowCode == "M"),]
-
-mirror_flow_4418 = merge(x= export_4418[c("cmdCode","period","reporterDesc","partnerDesc","primaryValue")],
-                    y= import_4418[c("cmdCode","period","reporterDesc","partnerDesc","primaryValue")],
-                    by.x=c("cmdCode", "period", "reporterDesc", "partnerDesc"),
-                    by.y=c("cmdCode", "period", "partnerDesc", "reporterDesc"),
-                    all.x = TRUE, all.y = TRUE)
-View(mirror_flow_4418)
-sum(is.na(mirror_flow_4418))
 
 #####IMPORTATION DU JEU DE DONNNEES 4412#####
 
@@ -205,25 +166,9 @@ write.csv(trade_data_3,
           "./raw_data/data_4412.csv",
           row.names = FALSE)
 
-# To load back the csv file
-data3 = read.csv2("./raw_data/data_4412.csv",sep = ",")
-
 all(dim(data3) == dim(trade_data_3)) # if TRUE: same dimensions
 # The two dataframes are not exactly equals: some minor differences remain
 # To check differences, use: all.equal(data, trade_data)
-
-###Jointure des deux tableaux pour observer les données miroir###
-##On remarque qu'il y'a de nombreuses NA dans le jeu miroir
-export_4412 = data3[(data3$flowCode == "X"),]
-import_4412 = data3[(data3$flowCode == "M"),]
-
-mirror_flow_4412 = merge(x= export_4412[c("cmdCode","period","reporterDesc","partnerDesc","primaryValue")],
-                    y= import_4412[c("cmdCode","period","reporterDesc","partnerDesc","primaryValue")],
-                    by.x=c("cmdCode", "period", "reporterDesc", "partnerDesc"),
-                    by.y=c("cmdCode", "period", "partnerDesc", "reporterDesc"),
-                    all.x = TRUE, all.y = TRUE)
-View(mirror_flow_4412)
-sum(is.na(mirror_flow_4412))
 
 #####IMPORTATION DU JEU DE DONNEES 4410#####
 
@@ -274,25 +219,9 @@ write.csv(trade_data_4,
           "./raw_data/data_4410.csv",
           row.names = FALSE)
 
-# To load back the csv file
-data4 = read.csv2("./raw_data/data_4410.csv",sep = ",")
-
 all(dim(data4) == dim(trade_data_4)) # if TRUE: same dimensions
 # The two dataframes are not exactly equals: some minor differences remain
 # To check differences, use: all.equal(data, trade_data)
-
-###Jointure des deux tableaux pour observer les données miroir###
-##On remarque qu'il y'a de nombreuses NA dans le jeu miroir
-export_4410 = data4[(data4$flowCode == "X"),]
-import_4410 = data4[(data4$flowCode == "M"),]
-
-mirror_flow_4410 = merge(x= export_4410[c("cmdCode","period","reporterDesc","partnerDesc","primaryValue")],
-                         y= import_4410[c("cmdCode","period","reporterDesc","partnerDesc","primaryValue")],
-                         by.x=c("cmdCode", "period", "reporterDesc", "partnerDesc"),
-                         by.y=c("cmdCode", "period", "partnerDesc", "reporterDesc"),
-                         all.x = TRUE, all.y = TRUE)
-View(mirror_flow_4410)
-sum(is.na(mirror_flow_4410))
 
 #####IMPORTATION DU JEU DE DONNEES 4406#####
 
@@ -343,40 +272,110 @@ write.csv(trade_data_5,
           "./raw_data/data_4406.csv",
           row.names = FALSE)
 
-# To load back the csv file
-data5 = read.csv2("./raw_data/data_4406.csv",sep = ",")
-
 all(dim(data5) == dim(trade_data_5)) # if TRUE: same dimensions
 # The two dataframes are not exactly equals: some minor differences remain
 # To check differences, use: all.equal(data, trade_data)
 
-###Jointure des deux tableaux pour observer les données miroir###
-##On remarque qu'il y'a de nombreuses NA dans le jeu miroir
+#########################################################################
+#########################################################################
+
+#####MISE EN FORME DES JEUX DE DONNEES ET EXPLOITATION DES INFORMATIONS#####
+
+###RAPPEL DES JEUX DE DONNEES###
+data = read.csv2("./raw_data/data_440131.csv",sep = ",")
+data2 = read.csv2("./raw_data/data_4418.csv",sep = ",")
+data3 = read.csv2("./raw_data/data_4412.csv",sep = ",")
+data4 = read.csv2("./raw_data/data_4410.csv",sep = ",")
+data5 = read.csv2("./raw_data/data_4406.csv",sep = ",")
+
+###NETTOYAGE DES DONNEES###
+toMatch = c('\\d','XX','_X')
+
+data = data[!(grepl(paste(toMatch,collapse="|"), data$reporterISO)) &
+       !(grepl(paste(toMatch,collapse="|"), data$partnerISO)),]
+
+data2 = data2[!(grepl(paste(toMatch,collapse="|"), data2$reporterISO)) &
+       !(grepl(paste(toMatch,collapse="|"), data2$partnerISO)),]
+
+data3 = data3[!(grepl(paste(toMatch,collapse="|"), data3$reporterISO)) &
+       !(grepl(paste(toMatch,collapse="|"), data3$partnerISO)),]
+
+data4 = data4[!(grepl(paste(toMatch,collapse="|"), data4$reporterISO)) &
+       !(grepl(paste(toMatch,collapse="|"), data4$partnerISO)),]
+
+data5 = data5[!(grepl(paste(toMatch,collapse="|"), data5$reporterISO)) &
+       !(grepl(paste(toMatch,collapse="|"), data5$partnerISO)),]
+
+
+###RAPPEL DES TABLEAUX IMPORT/EXPORT###
+export_440131 = data[(data$flowCode == "X"),]
+import_440131 = data[(data$flowCode == "M"),]
+
+export_4418 = data2[(data2$flowCode == "X"),]
+import_4418 = data2[(data2$flowCode == "M"),]
+
+export_4412 = data3[(data3$flowCode == "X"),]
+import_4412 = data3[(data3$flowCode == "M"),]
+
+export_4410 = data4[(data4$flowCode == "X"),]
+import_4410 = data4[(data4$flowCode == "M"),]
+
 export_4406 = data5[(data5$flowCode == "X"),]
 import_4406 = data5[(data5$flowCode == "M"),]
+
+###RAPPEL DES DONNEES MIROIR###
+mirror_flow_440131 = merge(x= export_440131[c("cmdCode","period","reporterDesc","partnerDesc","primaryValue")],
+                           y= import_440131[c("cmdCode","period","reporterDesc","partnerDesc","primaryValue")],
+                           by.x=c("cmdCode", "period", "reporterDesc", "partnerDesc"),
+                           by.y=c("cmdCode", "period", "partnerDesc", "reporterDesc"),
+                           all.x = TRUE, all.y = TRUE)
+sum(is.na(mirror_flow_440131))
+
+mirror_flow_4418 = merge(x= export_4418[c("cmdCode","period","reporterDesc","partnerDesc","primaryValue")],
+                         y= import_4418[c("cmdCode","period","reporterDesc","partnerDesc","primaryValue")],
+                         by.x=c("cmdCode", "period", "reporterDesc", "partnerDesc"),
+                         by.y=c("cmdCode", "period", "partnerDesc", "reporterDesc"),
+                         all.x = TRUE, all.y = TRUE)
+sum(is.na(mirror_flow_4418))
+
+mirror_flow_4412 = merge(x= export_4412[c("cmdCode","period","reporterDesc","partnerDesc","primaryValue")],
+                         y= import_4412[c("cmdCode","period","reporterDesc","partnerDesc","primaryValue")],
+                         by.x=c("cmdCode", "period", "reporterDesc", "partnerDesc"),
+                         by.y=c("cmdCode", "period", "partnerDesc", "reporterDesc"),
+                         all.x = TRUE, all.y = TRUE)
+sum(is.na(mirror_flow_4412))
+
+mirror_flow_4410 = merge(x= export_4410[c("cmdCode","period","reporterDesc","partnerDesc","primaryValue")],
+                         y= import_4410[c("cmdCode","period","reporterDesc","partnerDesc","primaryValue")],
+                         by.x=c("cmdCode", "period", "reporterDesc", "partnerDesc"),
+                         by.y=c("cmdCode", "period", "partnerDesc", "reporterDesc"),
+                         all.x = TRUE, all.y = TRUE)
+sum(is.na(mirror_flow_4410))
 
 mirror_flow_4406 = merge(x= export_4406[c("cmdCode","period","reporterDesc","partnerDesc","primaryValue")],
                          y= import_4406[c("cmdCode","period","reporterDesc","partnerDesc","primaryValue")],
                          by.x=c("cmdCode", "period", "reporterDesc", "partnerDesc"),
                          by.y=c("cmdCode", "period", "partnerDesc", "reporterDesc"),
                          all.x = TRUE, all.y = TRUE)
-View(mirror_flow_4406)
 sum(is.na(mirror_flow_4406))
 
+###CREATION D'UN VECTEUR UE27 REGROUPANT LES 27 PAYS MEMBRES DE L'UE###
+UE_27 = c('Austria','Belgium','Bulgaria','Croatia','Cyprus',
+          'Czech Republic','Denmark','Estonia','Finland','France',
+          'Germany','Greece','Hungary','Ireland','Italy','Latvia',
+          'Lithuania','Luxembourg','Malta','Netherlands','Poland',
+          'Portugal','Romania','Slovakia','Slovenia','Spain','Sweden')
+###CREATION DES DIFFERENTS SOUS-ENSEMBLES POUR ANALYSE###
 
-#########################################################################
-#########################################################################
-#####MISE EN FORME DES JEUX DE DONNEES ET EXPLOITATION DES INFORMATIONS#####
+##PRODUIT 440131##
+pattern <- paste(UE_27, collapse="|")
+UE_27_440131_interne <- data[grepl(pattern, data$reporterDesc) & 
+                       grepl(pattern,data$partnerDesc), ]
+###vérifier que la compréhension des données est bonne, est ce qu'on peut 
+## effectuer ce tri à partir du tableau des données miroir ou faut-il se baser
+## sur les tableaux data nettoyés? 
+ue_27_commerce <- data[grepl(pattern,data$reporterDesc)&
+                         !(grepl(pattern,data$partnerDesc)),]
+monde_europe_commerce <- data[!(grepl(pattern,data$reporterDesc))&
+                                !(grepl(pattern,data$partnerDesc)),]
 
-###RAPPEL DES JEUX DE DONNEES###
-
-###CREATION D'UN VECTEUR UE27 REGROUPANT LES 27 PAYS MEMBRES DE L'UE
-UE_27 = c("Austria","Belgium","Bulgaria","Croatia","Cyprus",
-          "Czech Republic","Denmark"," Estonia","Finland"," France",
-          "Germany","Greece","Hungary","Ireland","Italy","Latvia",
-          "Lithuania","Luxembourg","Malta"," Netherlands","Poland",
-          "Portugal","Romania","Slovakia","Slovenia"," Spain","Sweden")
-UE_27_440131= data[(data$reporterDesc==UE_27),]
-if (data[(data$partnerDesc==UE_27),]) {
-  sum(data[(data$partnerDesc),])
-}
